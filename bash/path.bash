44aaec52da844c0a81d0ae146621d0cd8582c688
ronhng <ronhng@qq.com>
2013-04-20 12:15:56 +0800

    tweak for mac

---
+++ bash/path.bash
@@ -0,0 +1,7 @@
+export PATH=$PATH:~/bin
+
+export PATH=${PATH}:/usr/local/git/bin:/usr/local/libexec/git-core
+export PATH=/usr/local/git/bin:$PATH
+export PATH=$PATH:/opt/local/bin
+export PATH=$PATH:~/bin
+export PATH=$PATH:/usr/local/mysql/bin
