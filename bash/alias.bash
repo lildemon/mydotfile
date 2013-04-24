b84db187af973c409d59d6b50b2a03f31fb94ea9
ronhng <ronhng@qq.com>
2013-04-24 16:13:57 +0800

    1. tweak for MsysGit Chinese input and showing support 2. new Bash command, d and p, which present a directory stack

--- bash/alias.bash
+++ bash/alias.bash
@@ -19,12 +19,14 @@ alias g='mvim --remote-silent'
 if [[ "$(uname -s)" == MINGW32* ]]; then
 	alias open=start
 	alias chrome="open chrome"
+	alias ls='ls -F --color --show-control-chars'
 elif [[ "$(uname -s)" == Darwin ]]; then
 	alias chrome='open -a "Google Chrome"'
 fi
 
 # 用sublime 打开 $DOTFILESDIR
 alias e="subl $DOTFILESDIR"
+alias suble="subl $DOTFILESDIR"
 
 alias vps="ssh ronhng@vps.frontkit.net -p 2843"
 alias kissy="chrome https://github.com/kissyteam/kissy"
