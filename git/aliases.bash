10587e1e45cc9bfcfcf5406206ed04a981e30e0b
ronhng <ronhng@qq.com>
2013-08-02 18:09:14 +0800

    difftool width beyondcompare

--- git/aliases.bash
+++ git/aliases.bash
@@ -18,3 +18,9 @@ alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+
+fndiffadded() {
+	git difftool -w $(git rev-list HEAD --reverse -- $1 | head -1)..HEAD -- $1;
+}
+
+alias diffadded=fndiffadded
